c512b9fde848f54979f5ff9a46231448a172dcfb
abdullahzubeyiryildiz <zubeyir_55@hotmail.com>
2022-09-09 16:51:24 +0300

    add

--- config/setting-modul.php
+++ config/setting-modul.php
@@ -1,10 +1,7 @@
 <?php
 
 
-$settings = cache()->rememberForever("site_settings", 60, function()
-{
-    return \SettingModul\Models\Setting::pluck("value","key")->all();
-});
+return [
 
-return config()->set("site", $settings);
+];
 
--- src/Providers/SettingModulServiceProvider.php
+++ src/Providers/SettingModulServiceProvider.php
@@ -21,7 +21,12 @@ class SettingModulServiceProvider extends ServiceProvider
 
         ], 'settingmodul');
 
+        $settings = cache()->rememberForever("site_settings", 60, function()
+        {
+            return \SettingModul\Models\Setting::pluck("value","key")->all();
+        });
 
+        return config()->set("site", $settings);
     }
 
     protected function configPath()
