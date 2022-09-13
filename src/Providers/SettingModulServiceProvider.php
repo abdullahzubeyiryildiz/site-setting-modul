979ba65786c50a9c06ae0f75f1c8d5eec2e12c37
abdullahzubeyiryildiz <zubeyir_55@hotmail.com>
2022-09-13 18:35:45 +0300

    Update SettingModulServiceProvider.php

--- src/Providers/SettingModulServiceProvider.php
+++ src/Providers/SettingModulServiceProvider.php
@@ -20,15 +20,16 @@ class SettingModulServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
         $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
-        $this->loadTranslationsFrom(__DIR__ . '/../../resources/lang', 'setting-modul');
-
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'settingmodul');
+        $this->loadTranslationsFrom(__DIR__ . '/../../resources/lang', 'settingmodul');
         Artisan::call('db:seed', [
             '--class' => SettingSeeder::class,
             '--force' => true // <--- add this line
         ]);
 
         $this->publishes([
-            __DIR__ . '/../../resources/lang' => base_path('resources/lang/vendor/setting-modul'),
+            __DIR__ . '/../../resources/views' => base_path('resources/views/vendor/settingmodul'),
+            __DIR__ . '/../../resources/lang' => base_path('resources/lang/vendor/settingmodul'),
             $this->configPath() => config_path('setting-modul.php'),
         ], 'settingmodul');
 
