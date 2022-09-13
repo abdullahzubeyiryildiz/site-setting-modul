18a65937f2437ce57986665e7e59273c5526bff5
abdullahzubeyiryildiz <zubeyir_55@hotmail.com>
2022-09-13 14:51:27 +0300

    add

--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -2,13 +2,10 @@
 
 namespace  SettingModul\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    use HasFactory;
-
     protected $fillable = ['title', 'key', 'value'];
 
 }
