b498d4c3f4e0577895a95b9a34eb939f124e5d75
abdullahzubeyiryildiz <zubeyir_55@hotmail.com>
2022-09-22 21:58:15 +0300

    Update SettingModulServiceProvider.php

--- src/Providers/SettingModulServiceProvider.php
+++ src/Providers/SettingModulServiceProvider.php
@@ -18,21 +18,22 @@ class SettingModulServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
         $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
-        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'settingmodul');
-        $this->loadTranslationsFrom(__DIR__ . '/../../resources/lang', 'settingmodul');
-        Artisan::call('db:seed', [
+
+       /*  Artisan::call('db:seed', [
             '--class' => SettingSeeder::class,
             '--force' => true // <--- add this line
         ]);
 
+       $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'settingmodul');
+        $this->loadTranslationsFrom(__DIR__ . '/../../resources/lang', 'settingmodul');
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
         $this->publishes([
             __DIR__ . '/../../resources/views' => base_path('resources/views/vendor/settingmodul'),
             __DIR__ . '/../../resources/lang' => base_path('resources/lang/vendor/settingmodul'),
             $this->configPath() => config_path('setting-modul.php'),
         ], 'settingmodul');
-
+        */
 
 
         //  config()->set("site", \SettingModul\Models\Setting::pluck("value","key")->all());
